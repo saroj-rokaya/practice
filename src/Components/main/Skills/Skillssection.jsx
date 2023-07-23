91fb97d59d2210a991aeb0728eb958eb42e8a504
Saroj Kumar Rokaya <110958773+saroj-rokaya@users.noreply.github.com>
2023-07-23 22:36:45 +0545

    minor

--- src/Components/main/Skills/Skillssection.jsx
+++ src/Components/main/Skills/Skillssection.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import "./skillssection.css";
+import React from 'react'
+import "./skillssection.css"
 
-export default function Skillssection() {
+export default function SkillsSection() {
   return (
-    <div className="skillscontainer">
-     
-      
-
+    <div className="skills-container">
+    <div className="skills-contents">Skills</div>
+    <div className="skill-no-1">
+      all skills
     </div>
-  );
+  </div>
+  )
 }
