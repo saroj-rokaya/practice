33d1ce43a9802e8cd0eeffdf7bbad51c09acc7ad
Saroj Kumar Rokaya <110958773+saroj-rokaya@users.noreply.github.com>
2023-07-27 18:31:37 +0545

    modify contact section

--- src/Components/main/Contacts/ContactsSection.jsx
+++ src/Components/main/Contacts/ContactsSection.jsx
@@ -9,8 +9,27 @@ export default function Contacts() {
       Contact
     </div>
 
-    <div className="contactsform">
-  
+    <div className="contactsform-container">
+      
+      <form>
+       <input type="text" name="fullName" placeholder="Enter Your Full Name" className="boxes" required/><br />
+       
+       <input type="text" name="address" placeholder="Enter Address" className="boxes" required/><br />
+       
+       <input type="tel" name="number" placeholder="Enter Mobile Number" className="boxes" required-pattern="[0-9]+" required/><br />
+
+       <input type="email" name="email" placeholder="Enter Email" className="boxes" required/><br />
+       
+       <textarea name="textarea"  cols="30" rows="5" placeholder="Write a comment" className="boxes" ></textarea><br />
+
+      <button className='submitBtn'>submit &#8594;</button>
+
+      </form>
+
+
+       
+
+      
       
     </div>
 
