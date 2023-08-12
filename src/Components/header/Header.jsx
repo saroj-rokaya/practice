f0a1bf737e2cb3345940cac4769f4a3a7df25e1e
Saroj Kumar Rokaya <110958773+saroj-rokaya@users.noreply.github.com>
2023-08-12 21:40:43 +0545

    responsive start

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -4,20 +4,21 @@ import CV from "./CV.pdf"
 
 export default function Header() {
   return (
-    <div className="header">
+    <div className="header-container">
         <nav>
             <div className="namecontainer">
             <p className="name">Saroj Kumar Rokaya</p>
             </div>
             <ul className="header-nav-lists">
-                <li className="navlists">Home</li>
-                <li className="navlists">About</li>
-                <li className="navlists">Skills</li>
-                <li className="navlists">Projects</li>
-                <li className="navlists">Contact</li>
+                <li className="header-navlists">Home</li>
+                <li className="header-navlists">About</li>
+                <li className="header-navlists">Skills</li>
+                <li className="header-navlists">Projects</li>
+                <li className="header-navlists">Contact</li>
                 <a href={CV}>
-                <li className="navlists">CV</li>
+                <li className="header-navlists">CV</li>
                 </a>
+                <i class="ri-menu-fill"></i>
             </ul>
         </nav>
     </div>
--- src/Components/main/Skills/Skillssection.jsx
+++ src/Components/main/Skills/Skillssection.jsx
@@ -2,48 +2,21 @@ import React, { useState } from 'react'
 import "./skillssection.css"
 
 export default function SkillsSection() {
-  const [cards] = useState([
-     {
-      title: 'card-1',
-      text: ' dlfjdskf fjfdskfj dfdjfdjf dfjkdskf dsf dsfjdsf jdf dsfkjdslfjdsf;ds '
-     },
-     {
-      title: 'card-2',
-      text: ' dlfjdskf fjfdskfj dfdjfdjf dfjkdskf dsf dsfjdsf jdf dsfkjdslfjdsf;ds '
-     },
-     {
-      title: 'card-3',
-      text: ' dlfjdskf fjfdskfj dfdjfdjf dfjkdskf dsf dsfjdsf jdf dsfkjdslfjdsf;ds '
-     },
-     {
-      title: 'card-4',
-      text: ' dlfjdskf fjfdskfj dfdjfdjf dfjkdskf dsf dsfjdsf jdf dsfkjdslfjdsf;ds '
-     }
-
-  ])
   return (
     <div className="skills-container">
-    <div className="skills-contents">Skills</div>
-
-    <div className="skills-card-container">
-      {/* <div className="skills-by-cards"> */}
-        <div className="cards">
-            {
-              cards.map((card,i)=>{
-                <div key={i} className="card">
-                  <h3>
-                    {card.title}
-                  </h3>
-                  <p>
-                    {card.text}
-                  </p>
-                </div>
-
-              })
-            }
-        </div>
-      {/* </div> */}
+      <div className="left-skills-section">
+        Skills
+      </div>
+      <div className="right-skills-section">
+        <div className="skill 1st-skill">1st</div>
+        <div className="skill 2nd-skill">2nd</div>
+        <div className="skill 3rd-skill">3rd</div>
+        <div className="skill 4th-skill">4th</div>
+        <div className="skill 5th-skill">5th</div>
+        <div className="skill 5th-skill">5th</div>
+        <div className="skill 5th-skill">5th</div>
+        <div className="skill 5th-skill">5th</div>
+      </div>
     </div>
-  </div>
-  )
-}
+  
+)}
